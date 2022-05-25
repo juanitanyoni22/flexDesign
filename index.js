8965b011107da66f55a459ae3280d96019e49a11
2217lizz <sakalacourage@gmail.com>
2022-05-25 11:44:16 +0200

    added new classes in html , added media queries in css and created a typogpaphy.css file

---
+++ index.js
@@ -0,0 +1,6 @@
+function myFunction{
+   let x= document.getElementById("mainHeading")
+    innerHTML="My journey as a software enginer";
+    x.style.fontSize="16px"
+    x.style.color="98, 149, 149"
+}
